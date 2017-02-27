c5479fc8ca8d39bac7cde5f0873b4ed0d9ac7fe7
Adrian <adrpieper@gmail.com>
2017-02-27 20:38:16 +0100

    Gotowy projekt Family

--- src/family/Family.java
+++ src/family/Family.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 public class Family {
 
     public static void main(String[] args) {
+
         Mother mother = new Mother("Grażyna");
         Father father = new Father("Janusz");
         Son son = new Son("Sebastian");
@@ -16,35 +17,8 @@ public class Family {
         FamilyMember[] members = new FamilyMember[]{mother,father,son,daugther};
         System.out.println("Rodzina: ");
         for (FamilyMember member : members) {
-            introduce(member);
+            member.introduceYourself();
         }
-    }
 
-    public static void introduce(FamilyMember member) {
-        if (member instanceof Mother) {
-            introduce((Mother) member);
-        }
-        else if (member instanceof Father) {
-            introduce((Father) member);
-        }
-        else if (member instanceof Son) {
-            introduce((Son) member);
-        }
-        else if (member instanceof Daugther) {
-            introduce((Daugther) member);
-        }
-    }
-
-    public static void introduce(Mother mother){
-        System.out.println("I’m a mother. My name is " + mother.getName());
-    }
-    public static void introduce(Father father) {
-        System.out.println("I’m a father. My name is " + father.getName());
-    }
-    public static void introduce(Son son){
-        System.out.println("I’m a son. My name is " + son.getName());
-    }
-    public static void introduce(Daugther daugther){
-        System.out.println("I’m a daugther. My name is " + daugther.getName());
     }
 }
