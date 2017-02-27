c9fed2b93a06798e75f358fd2502629a98e0d33c
Adrian <adrpieper@gmail.com>
2017-02-27 18:35:53 +0100

    Utworzyłem rodzinę

---
+++ src/family/Family.java
@@ -0,0 +1,15 @@
+package family;
+
+/**
+ * Created by Adrian on 2017-02-27.
+ */
+public class Family {
+
+    public static void main(String[] args) {
+        Mother mother = new Mother("Grażyna");
+        Father father = new Father("Janusz");
+        Son son = new Son("Sebastian");
+        Daugther daugther = new Daugther("Angelika");
+
+    }
+}
