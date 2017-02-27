d815450ec48723f5b2c32b46d7357efeae165e43
Adrian <adrpieper@gmail.com>
2017-02-27 18:42:34 +0100

    Utworzyłem metody introduce

--- src/family/Family.java
+++ src/family/Family.java
@@ -12,4 +12,17 @@ public class Family {
         Daugther daugther = new Daugther("Angelika");
 
     }
+
+    public static void introduce(Mother mother){
+        System.out.println("I’m a mother. My name is " + mother.getName());
+    };
+    public static void introduce(Father father){
+        System.out.println("I’m a father. My name is " + father.getName());
+    };
+    public static void introduce(Son son){
+        System.out.println("I’m a son. My name is " + son.getName());
+    };
+    public static void introduce(Daugther daugther){
+        System.out.println("I’m a daugther. My name is " + daugther.getName());
+    };
 }
