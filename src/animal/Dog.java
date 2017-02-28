9d66681d2a57ac225afb34da6ea4260706135e4c
Adrian <adrpieper@gmail.com>
2017-02-28 18:31:18 +0100

    Dodałem wszystkie zwierzęta

---
+++ src/animal/Dog.java
@@ -0,0 +1,11 @@
+package animal;
+
+/**
+ * Created by Adrian on 2017-02-28.
+ */
+public class Dog extends Animal {
+    @Override
+    public void giveSound() {
+        System.out.println("Hau, Hau!");
+    }
+}
---
+++ src/animal/Monkey.java
@@ -0,0 +1,12 @@
+package animal;
+
+/**
+ * Created by Adrian on 2017-02-28.
+ */
+public class Monkey extends Animal {
+
+    @Override
+    public void giveSound() {
+        System.out.println("Monkey sound...");
+    }
+}
